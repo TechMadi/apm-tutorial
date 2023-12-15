8467f256dbbf7f9bd6ad527f24cb57f164eec426
TechMadi <winnieamandela@gmail.com>
2023-12-15 21:17:13 +0300

    Nav Set Up complete

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import {WelcomeComponent} from "./home/welcome/welcome.component";
+import {ShellComponent} from "./home/shell/shell.component";
+import {PageNotFoundComponent} from "./home/page-not-found/page-not-found.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ShellComponent,
+    children: [
+      { path: 'welcome', component: WelcomeComponent },
+      {
+        path: 'products',
+        // canActivate: [AuthGuard],
+        loadChildren: ()=> import('./product/product.module').then(m=>m.ProductModule)
+      },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+    ]
+  },
+  {
+    path:"**",component:PageNotFoundComponent
+  }
+];
