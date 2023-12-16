38ab1580513bf02bb4b326a999224a95a33be3b9
TechMadi <winnieamandela@gmail.com>
2023-12-16 11:20:09 +0300

    Product List Complete

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {IProduct} from "../product";
+import {ProductService} from "../product.service";
 
 @Component({
   selector: 'app-product-list',
@@ -7,6 +9,54 @@ import { Component } from '@angular/core';
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{
+pageTitle:string='Product List'
+  showImage:boolean=true;
 
+imageWidth:number=50;
+imageMargin:number=2;
+errorMessage:string=""
+
+  private  _listFilter:string="";
+
+get listFilter():string{
+   return this._listFilter
 }
+
+set listFilter(value){
+  this._listFilter=value;
+  this.performFilter(this.listFilter)
+}
+
+filteredProducts:IProduct[]=[]
+products:IProduct[]=[]
+
+  constructor(private  productService:ProductService) {
+  }
+
+  ngOnInit():void{
+  this.productService.getProducts().subscribe(
+    (products:IProduct[])=>{
+      this.products=products;
+      this.performFilter(this.listFilter)
+
+    },
+    (error:any)=>{
+      this.errorMessage=<any>error
+    }
+  )
+  }
+
+  toggleImage():void{
+  this.showImage=!this.showImage
+  }
+  performFilter(filterBy?:string):void{
+
+  if(filterBy){
+    this.filteredProducts=this.products.filter((product)=>product.productName.toLocaleLowerCase().indexOf(filterBy.toLocaleLowerCase())!==-1);
+  }else{
+    this.filteredProducts=this.products
+  }
+  }
+}
+
